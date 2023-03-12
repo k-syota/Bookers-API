a51268e6a9dbe055bf767cfb9ea9b9c32f3bd5ec
Syota Nishimura <syota4723@gmail.com>
2023-03-12 18:49:43 +0900

    :sparkles: AuthControllerを作成

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        # code...
+    }
+
+    public function login(LoginRequest $request)
+    {
+        # code...
+    }
+}
