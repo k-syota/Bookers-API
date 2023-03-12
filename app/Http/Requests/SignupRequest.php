47d5755dcb9a899387cf8cde85c50969831a5194
Syota Nishimura <syota4723@gmail.com>
2023-03-12 18:50:54 +0900

    :sparkles: ログイン・アカウント登録用のRequestを作成

---
+++ app/Http/Requests/SignupRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rules\Password;
+
+class SignupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)->mixedCase()->numbers()->symbols()
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response['errors']  = $validator->errors()->toArray();
+
+        throw new HttpResponseException(response()->json($response));
+    }
+}
